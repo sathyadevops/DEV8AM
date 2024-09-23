5dc095f165fc24f83b3741250cd64fc58a78adbd
sathyadevops <sathyadevops9@gmail.com>
2024-09-23 03:28:54 +0000

    bank code

---
+++ bank.java
@@ -0,0 +1,10 @@
+class Bank
+{
+  public static void main(String []args)
+  {
+     System.out.println(" Test code");
+     System.out.println(" Test code");
+     System.out.println(" Test code");
+     System.out.println(" Test code");
+  }
+}
